0c8e938cba831f66742df27f2a21f66de1ff7fc6
Tiffani Underwood <tdunderwood217@gmail.com>
2023-10-07 19:06:10 -0400

    test for deployment

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//     content: ["./src/**/*.{html,js}"],
+//     theme: {
+//         colors: {
+//             'pink': '#ff49db'
+//         }
+//       extend: {},
+//     },
+//     plugins: [],
+// }
+  
